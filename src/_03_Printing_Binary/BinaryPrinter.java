e04cda74265b5ec61d1873e2cd41874489ba23da
dalidog <daliasebat@gmail.com>
2019-11-24 11:30:59 -0800

    finished printing binary

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -6,35 +6,89 @@ public class BinaryPrinter {
 	//Do not use the Integer.toBinaryString method!
 	//Don't be afraid to use the methods that are already complete to finish the others.
 	//Create a main method to test your methods.
-	int x = 0;
-	String result;
+	
+	
 	public static void main(String[] args) {
 		printByteBinary((byte) 4);
+		printShortBinary((short)64);
+		printIntBinary((int) 28);
+		printLongBinary((long) 200);
 	}
 	
 	public static String printByteBinary(byte b) {
-	while (b!=0) {
-		b=(byte)(b/2);
-		x=x+1;
-	}
-	result = "1";
-	for (int i = 0; i < x; i++) {
-		result = result + "0";
-	}
-	System.out.println(result);
-	return result;
+		String result=""; 
+		String result2="";
+		for (int i = 0; i < 8; i++) {
+		if(b%2==0) {
+			result+="0";
+		}
+		else {
+			result+="1";
+		}
+		b>>=1;
+		}
+		for (int i = result.length()-1; i >= 0; i--) {
+			result2=result2+result.charAt(i);
+		}
+		System.out.println(result2);
+		return result2;
 	
 	}
 	
-	public void printShortBinary(short s) {
-		
+	public static String printShortBinary(short s) {
+		String result=""; 
+		String result2="";
+		for (int i = 0; i < 16; i++) {
+		if(s%2==0) {
+			result+="0";
+		}
+		else {
+			result+="1";
+		}
+		s>>=1;
+		}
+		for (int i = result.length()-1; i >= 0; i--) {
+			result2=result2+result.charAt(i);
+		}
+		System.out.println(result2);
+		return result2;
 	}
 	
-	public void printIntBinary(int i) {
-		
+	public static String printIntBinary(int i) {
+		String result=""; 
+		String result2="";
+		for (int z = 0; z < 32 ; z++) {
+		if(i%2==0) {
+			result+="0";
+		}
+		else {
+			result+="1";
+		}
+		i>>=1;
+		}
+		for (int x = result.length()-1; x >= 0; x--) {
+			result2=result2+result.charAt(x);
+		}
+		System.out.println(result2);
+		return result2;
 	}
 	
-	public void printLongBinary(long l) {
-		
+	public static String printLongBinary(long l) {
+		String result=""; 
+		String result2="";
+		for (int z = 0; z < 64 ; z++) {
+		if(l%2==0) {
+			result+="0";
+		}
+		else {
+			result+="1";
+		}
+		l>>=1;
+		}
+		for (int x = result.length()-1; x >= 0; x--) {
+			result2=result2+result.charAt(x);
+		}
+		System.out.println(result2);
+		return result2;
 	}
 }
